54d253439425a1eff76ef55b0ef62cd114bf84bc
Steven Williams <evilgeniusnyc@me.com>
2020-07-19 18:50:56 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -63,6 +63,9 @@ epic_tragedy[:additional_characters] = []
 prince = { name: "Prince Escalus" }
 apothecary = { name: "Apothecary" }
 
+epic_tragedy[:additional_characters] << prince
+epic_tragedy[:additional_characters] << apothecary
+
 
 
 
